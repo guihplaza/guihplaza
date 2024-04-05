5a161a4ff8035c3b87a301d80c7e96420584b5b6
Guilherme Plaza Galoro <guihplaza@gmail.com>
2024-04-05 11:19:33 -0300

    RPA
    Desenvolvimento de um RPA para teste de carga básico no formulário do departamento de água e esgoto de Marília.

---
+++ RPA - Daem/RPA - Daem/AuthomationWeb.cs
@@ -0,0 +1,51 @@
+﻿using Bogus;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPADaem
+{
+    public class AuthomationWeb
+    {
+        public IWebDriver driver;
+        public AuthomationWeb()
+        {
+            this.driver = new EdgeDriver();
+
+        }
+
+        public void Runner()
+        {
+            var random = new Random();         
+            var faker = new Faker();         
+            var nome = faker.Name.FullName();
+            var email = $"{nome.Replace(" ", "")}{random.Next(1000)}@gmail.com";
+            var celular = faker.Phone.PhoneNumber();
+            var cidade = faker.Address.City();
+            var textoAleatorio = faker.Lorem.Sentence();
+
+            driver.Navigate().GoToUrl("https://www.daem.com.br/contato/index");
+
+            var combo = driver.FindElement(By.XPath("//*[@id=\"departamento\"]"));//combo departamento
+            var selectElement = new SelectElement(combo);
+
+            selectElement.SelectByIndex(8);
+
+            driver.FindElement(By.XPath("//*[@id=\"nome\"]")).SendKeys(nome);//nome
+            driver.FindElement(By.XPath("//*[@id=\"email\"]")).SendKeys(email);//email
+            driver.FindElement(By.XPath("//*[@id=\"cidade\"]")).SendKeys(cidade);//cidade
+            driver.FindElement(By.XPath("//*[@id=\"estado\"]")).SendKeys("SP");//Estado
+            driver.FindElement(By.XPath("//*[@id=\"telefone\"]")).SendKeys(celular);//telefone
+            driver.FindElement(By.XPath("//*[@id=\"msg\"]")).SendKeys(textoAleatorio);//mensagem
+
+
+            driver.FindElement(By.XPath("//*[@id=\"btnSubmit\"]")).Click(); //botao final de enviar
+
+        }
+    }
+}
---
+++ RPA - Daem/RPA - Daem/Program.cs
@@ -0,0 +1,13 @@
+﻿using RPADaem;
+
+var web = new AuthomationWeb();
+
+// Loop infinito para execução contínua
+while (true)
+{
+    // Aqui você pode colocar o código que deseja executar repetidamente
+    web.Runner();
+
+    // Espera de 0.1 segundo antes da próxima iteração
+    Thread.Sleep(1);
+}
